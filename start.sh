f98723f88b3efce16e6d73a5a3d0d3291f944885
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2024-12-27 15:44:17 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/MBREDOIL/Ultra-Forward-Bot MBREDOIL/Ultra-Forward-Bot
+  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot JishuDeveloper/Ultra-Forward-Bot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/MBREDOIL/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
-if
+  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
+fi
 cd JishuDeveloper/Ultra-Forward-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
