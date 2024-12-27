9bae386ed5b5c9ceeabee351087e16e3847bfdff
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2024-12-27 15:17:12 +0530

    config.py

--- config.py
+++ config.py
@@ -9,12 +9,12 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = os.environ.get("API_ID", "22182189")
+    API_HASH = os.environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "https://t.me/UIHASH")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+    DB_URL = os.environ.get("DB_URL", "")
+    DB_NAME = os.environ.get("DB_NAME", "uio21bot")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6106998329').split()]
 
 
